f1f41ca25ca15ef9588fcc3cf1e1dcb95cf4e367
SrTorres07 <srtorresfelipe07@outlook.com>
2025-01-13 12:14:20 +0000

    poker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let suits = ["♦", "♥", "♠ ", "♣"];
+
+  let valuesRandom = values[Math.floor(Math.random() * values.length)];
+  let suitsRandom = suits[Math.floor(Math.random() * suits.length)];
+
+  let card = document.getElementById("card");
+  let icon_top = document.getElementById("icon_top");
+  let icon_central = document.getElementById("icon_central");
+  let icon_bottom = document.getElementById("icon_bottom");
+
+  if (suitsRandom === "♥" || suitsRandom === "♦") {
+    card.className = "cardred";
+  } else {
+    card.className = "card";
+  }
+
+  icon_central.innerHTML = valuesRandom;
+  icon_top.innerHTML = suitsRandom;
+  icon_bottom.innerHTML = suitsRandom;
 };
